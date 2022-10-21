5953b1fd3c98b5e3bba0c9c4ffdd84f128782812
sebastianbavini <sebastianbavini@hotmail.com>
2022-10-21 20:40:20 -0300

    vai dar certo

--- app/src/main/java/com/goodbit/opportunities/AnuncioActivity.kt
+++ app/src/main/java/com/goodbit/opportunities/AnuncioActivity.kt
@@ -3,11 +3,14 @@ package com.goodbit.opportunities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.goodbit.opportunities.databinding.ActivityAnuncioBinding
+import com.goodbit.opportunities.viewmodel.AnuncioViewModel
 
 class AnuncioActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityAnuncioBinding
+    private lateinit var viewModel:AnuncioViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,7 @@ class AnuncioActivity : AppCompatActivity(), View.OnClickListener {
         binding=ActivityAnuncioBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel= ViewModelProvider(this).get(AnuncioViewModel::class.java)
        binding.butonSalvarAnuncio.setOnClickListener(this)
 
     }
