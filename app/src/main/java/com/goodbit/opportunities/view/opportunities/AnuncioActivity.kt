33e8ed86c10c93ea6a26c08ce613097606c8ca57
sebastianbavini <sebastianbavini@hotmail.com>
2022-10-21 21:45:20 -0300

    mudanças nos icones

--- app/src/main/java/com/goodbit/opportunities/view/opportunities/AnuncioActivity.kt
+++ app/src/main/java/com/goodbit/opportunities/view/opportunities/AnuncioActivity.kt
@@ -4,21 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
-<<<<<<< HEAD:app/src/main/java/com/goodbit/opportunities/AnuncioActivity.kt
-=======
 import com.goodbit.opportunities.R
->>>>>>> 3da1be7cf3008f4f6858e5635cd570733a0ce6cf:app/src/main/java/com/goodbit/opportunities/view/opportunities/AnuncioActivity.kt
+
 import com.goodbit.opportunities.databinding.ActivityAnuncioBinding
 import com.goodbit.opportunities.viewmodel.AnuncioViewModel
 
 class AnuncioActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityAnuncioBinding
-<<<<<<< HEAD:app/src/main/java/com/goodbit/opportunities/AnuncioActivity.kt
-    private lateinit var viewModel:AnuncioViewModel
-=======
     private lateinit var viewModel: AnuncioViewModel
->>>>>>> 3da1be7cf3008f4f6858e5635cd570733a0ce6cf:app/src/main/java/com/goodbit/opportunities/view/opportunities/AnuncioActivity.kt
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,14 +23,11 @@ class AnuncioActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 
         viewModel= ViewModelProvider(this).get(AnuncioViewModel::class.java)
-<<<<<<< HEAD:app/src/main/java/com/goodbit/opportunities/AnuncioActivity.kt
+
        binding.butonSalvarAnuncio.setOnClickListener(this)
-=======
 
         binding.butonSalvarAnuncio.setOnClickListener(this)
         binding.radioDemanda.isChecked=true
->>>>>>> 3da1be7cf3008f4f6858e5635cd570733a0ce6cf:app/src/main/java/com/goodbit/opportunities/view/opportunities/AnuncioActivity.kt
-
     }
 
     override fun onClick(v: View) {
